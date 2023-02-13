3778d87f9b15f4ca89e66313d47572624246f325
amrithasindhu <95172197+amrithasindhu@users.noreply.github.com>
2023-02-13 15:44:09 +0530

    Delete matrix.py

--- matrix.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-r=int(input("Enter the number of rows:"))
-c=int(input("Enter the number of columns:"))
-a=[]
-for i in range(r*c):
-        a.append(int(input()))
-a=np.array(a)
-a=a.reshape(r,c)
-print(a)
